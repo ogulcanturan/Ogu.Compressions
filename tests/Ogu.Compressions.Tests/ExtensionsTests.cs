216e447aff20a8b475f19a24053ff0255aabec36
Oğulcan TURAN <ismetogulcanturan@gmail.com>
2025-04-24 01:35:53 +0200

    Fixed unit test

--- tests/Ogu.Compressions.Tests/ExtensionsTests.cs
+++ tests/Ogu.Compressions.Tests/ExtensionsTests.cs
@@ -18,12 +18,12 @@ namespace Ogu.Compressions.Tests
             var noCompressionResult = CompressionLevel.NoCompression.ToZstdLevel();
 
             // Assert
-            Assert.Equal(Compressor.MaxCompressionLevel, optimalResult);
+            Assert.Equal(Compressor.DefaultCompressionLevel, optimalResult);
 #if NET6_0_OR_GREATER
             Assert.Equal(Compressor.MaxCompressionLevel, smallestSizeResult);
 #endif
             Assert.Equal(Compressor.MinCompressionLevel, fastestResult);
-            Assert.Equal(Compressor.DefaultCompressionLevel, noCompressionResult);
+            Assert.Equal(Compressor.MinCompressionLevel, noCompressionResult);
         }
     }
 }
